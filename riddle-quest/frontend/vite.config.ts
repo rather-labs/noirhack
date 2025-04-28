fc20758fda3fc2903f0fde3974bfb0b447a436cd
nvcho <ipmain07@gmail.com>
2025-04-28 13:09:11 -0300

    feat(frontend): add bb, noir, react router, and tailwind deps

--- riddle-quest/frontend/vite.config.ts
+++ riddle-quest/frontend/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  optimizeDeps: {
+    esbuildOptions: { target: "esnext" },
+    exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js']
+  },
+  plugins: [react(), tailwindcss()],
 })
