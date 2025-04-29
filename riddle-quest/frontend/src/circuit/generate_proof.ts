fcb59175ab46fe9f5e4bfaacf0f983c82ded2a9b
nvcho <ipmain07@gmail.com>
2025-04-29 14:12:03 -0300

    feat(frontend): please work

--- riddle-quest/frontend/src/circuit/generate_proof.ts
+++ riddle-quest/frontend/src/circuit/generate_proof.ts
@@ -1,10 +1,12 @@
-import circuit from "./riddle.json";
-import { Noir } from "@noir-lang/noir_js";
-import { ProofData, UltraHonkBackend } from "@aztec/bb.js";
+import circuit from './riddle.json';
+import { Noir } from '@noir-lang/noir_js';
+import { ProofData, UltraHonkBackend } from '@aztec/bb.js';
 
-export default async function generate_proof(guess: number[], expected_hash: string): Promise<ProofData> {
+export async function generate_proof(
+  guess: number[],
+  expected_hash: string
+): Promise<ProofData> {
   const fn = generate_proof.name;
-
 
   if (!guess.length) {
     throw new Error(`${fn}: received empty array ❌`);
@@ -15,26 +17,26 @@ export default async function generate_proof(guess: number[], expected_hash: str
   try {
     // @ts-expect-error – no ideeeeaaa woaaaaaaa
     const noir = new Noir(circuit);
-    const backend = new UltraHonkBackend(circuit.bytecode);
+    const honk_backend = new UltraHonkBackend(circuit.bytecode, { threads: 1 });
 
     console.log(`${fn}: generating witness… ⏳`);
     const { witness } = await noir.execute({ guess, expected_hash });
     console.log(`${fn}: witness generated ✅`);
 
     console.log(`${fn}: generating proof… ⏳`);
-    const proof = await backend.generateProof(witness);
+    const proof_data = await honk_backend.generateProof(witness, {
+      keccak: true,
+    });
     console.log(`${fn}: proof generated ✅`);
 
-    if (!backend.verifyProof(proof)) {
-      throw new Error("invalid proof");
+    if (!honk_backend.verifyProof(proof_data)) {
+      throw new Error('invalid proof');
     }
 
-    return proof;
+    return proof_data;
   } catch (err) {
     const message =
-      err instanceof Error
-        ? `${fn}: ${err.message}`
-        : `${fn}: unknown error`;
+      err instanceof Error ? `${fn}: ${err.message}` : `${fn}: unknown error`;
 
     throw new Error(message);
   }
