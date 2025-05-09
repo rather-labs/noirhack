e2edbe08885ae97476c8659a7e55cebfd7e56779
nvcho <ipmain07@gmail.com>
2025-05-09 20:36:47 +0100

    feat: contracts

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,38 @@
+import type { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox-viem';
+import 'hardhat-contract-sizer';
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: '0.8.28',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 500, // helps reduce bytecode size
+      },
+      evmVersion: 'cancun',
+      viaIR: false,
+    },
+  },
+  contractSizer: {
+    alphaSort: true, // Sort contracts alphabetically
+    runOnCompile: true, // Automatically measure size after compilation
+    disambiguatePaths: false, // Show file paths for contracts with the same name
+  },
+  networks: {
+    hardhat: {
+      chainId: 31_337,
+      mining: {
+        auto: true,
+        interval: 15_000,
+      },
+    },
+    localhost: {
+      //url: 'http://localhost:8546',
+      allowUnlimitedContractSize: true,
+      chainId: 31_337,
+    },
+  },
+};
+
+export default config;
