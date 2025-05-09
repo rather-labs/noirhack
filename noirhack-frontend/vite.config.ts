f6aaf71ea51a29bb48c246e80ed7a9cdb8a5014d
nvcho <ipmain07@gmail.com>
2025-05-09 17:44:11 +0100

    feat(frontend): AAAAAAA 2hs

--- noirhack-frontend/vite.config.ts
+++ noirhack-frontend/vite.config.ts
@@ -4,5 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: { target: 'esnext' },
+    exclude: ['@noir-lang/noirc_abi', '@noir-lang/acvm_js'],
+  },
   plugins: [react(), tailwindcss()],
 });
