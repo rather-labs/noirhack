7f9e70f13e437a0917d5f1f8ef8c28582c963ee9
Juan Beliera <jbeliera@ratherlabs.com>
2025-05-09 17:48:54 -0300

    fix: Scripts to generate verifier contract and json circuit

--- circuits/riddle_2/scripts/generateContract.sh
+++ circuits/riddle_2/scripts/generateContract.sh
@@ -2,4 +2,4 @@ nargo compile #
 bb write_vk --scheme ultra_honk -b ./target/riddle.json -o ./target --oracle_hash keccak #
 bb write_solidity_verifier --scheme ultra_honk -k ./target/vk -o ./target/riddleVerify.sol #
 mv ./target/riddleVerify.sol ../../contracts/contracts/RiddleVerifier.sol #
-mv ./target/riddleVerify.json ../../frontend/src/config/abi/RiddleVerifier.json #
+mv ./target/riddle.json ../../frontend/src/noir/circuits/riddle.json #
