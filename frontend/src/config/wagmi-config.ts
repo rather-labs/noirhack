f46f6ff4247dcc43302438d55320306a7360a2c8
nvcho <ipmain07@gmail.com>
2025-06-06 11:26:51 +0200

    feat: udpate wagmi-config

--- frontend/src/config/wagmi-config.ts
+++ frontend/src/config/wagmi-config.ts
@@ -1,17 +1,22 @@
-import { http, createConfig } from 'wagmi';
-import { mainnet, sepolia, hardhat } from 'wagmi/chains';
 import { injected } from 'wagmi/connectors';
+import { sepolia, hardhat } from 'wagmi/chains';
+import { http, createConfig } from 'wagmi';
+
+const SEPOLIA_RPC_URL = import.meta.env.VITE_SEPOLIA_RPC_URL as string;
+
+if (!SEPOLIA_RPC_URL) {
+  throw new Error('⛔ Missing environment variable: SEPOLIA_RPC_URL');
+}
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, sepolia, hardhat],
+  chains: [hardhat, sepolia],
   connectors: [
     injected({
       target: 'metaMask',
     }),
   ],
   transports: {
-    [mainnet.id]: http(),
-    [sepolia.id]: http(),
+    [sepolia.id]: http(SEPOLIA_RPC_URL),
     [hardhat.id]: http('http://127.0.0.1:8545', {
       timeout: 10_000,
       retryCount: 10,
